c19079db951df55c7a0e8d3ec87a3c76fdaf9d3c
student <aime.thierry97@gmail.com>
2019-11-26 15:33:58 +0100

    changed codes

--- receive.c
+++ receive.c
@@ -1,39 +1,55 @@
-#include<stdio.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/types.h>
-#include<string.h>
-#include<errno.h>
-#include<stdlib.h>
-
-#define SHSIZE 100
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+
+char * shm;
+char * s;
+
+int openfile(char * name);
+
+int openfile(char * name)
+{
+	int shmid = -1;
+	int size;
+	shmid = shm_open (name, O_RDWR, 0600);
+	if(shmid == -1){
+		perror("error when opening \n");
+	}
+	size = lseek(shmid, 0, SEEK_END);
+	shm = (char *) mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
+	
+	if (shm == MAP_FAILED)
+	{
+        perror ("ERROR: mmap() failed");
+    }
+	return 0;
+}
+
 
 int main(int argc, char * argv[])
 {
-	int shmid;
-	key_t key;
-	key = 9976;
-	char * shm;
-	char * s;
-	//shmid = shmget(key, SHSIZE,  0666| IPC_CREAT); shmget(key, SHSIZE , 0644|IPC_CREAT); 
-	shmid = shmget(key, SHSIZE, IPC_CREAT | 0666);
-	if(shmid < 0)
-	{
-		perror ("ERROR: shm_open() failed \n");
-		exit(1);
-	}
-	shm = shmat(shmid, NULL, 0);
-	if(shm == (char *) -1)
-	{
-		perror ("shmat \n");
-		exit(1);
-	}
+	char name[80];
+	
+	printf("enter a file u want to open \n");
+	fgets(name, sizeof (name), stdin);
+	openfile(name);
+	
+	/*
 	for(s = shm; *s != 0 ; s++)
 	{
 		printf("%c", *s);
 	}
-	printf("\n");
-	*shm = 'A';
+	*shm = 'A';*/
+	printf("address of the file %d", &shm);
 	return 0;
 }
--- shared.c
+++ shared.c
@@ -5,49 +5,86 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-
 #include <unistd.h>
-
 #include <stdlib.h>
 
-#define SHSIZE 100
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
 
-int main(int argc, char * argv[])
+
+//#define SHSIZE 100
+int create(char *name , int size);
+int dele_file(char * s);
+char * shm;
+char * s;
+
+int dele_file(char * s)
+{
+    int len;
+    len = strlen (s);
+    if (s [len - 1] == '\n')
+    {
+        s [len - 1] = '\0';
+    }
+    return 0;
+}
+
+int create(char * name, int SHSIZE)
 {
 	int shmid = -1;
-	key_t key;
-	key = 9976;
-	char * shm;
-	char * s;
-	//shmid = shmget(key, SHSIZE,  0666| IPC_CREAT); shmget(key, SHSIZE , 0644|IPC_CREAT); 
-   	shmid = shmget(key, SHSIZE, IPC_CREAT | 0666);
-	//shmid = shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, 0600);
+	int cmpr;
+   	shmid = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0600);
 	if(shmid == -1)
 	{
 		perror ("ERROR: shm_open() failed \n");
 		exit(1);
 	}
-	shm = shmat(shmid, NULL, 0);
-	if(shm == (char *) -1)
+	
+	cmpr =  ftruncate(shmid, SHSIZE);
+	shm = (char *) mmap (NULL, SHSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
+	
+	
+	if(shm == MAP_FAILED)
 	{
 		perror ("shmat \n");
 		exit(1);
 	}
-	memcpy(shm, "hello world", 11);
+	memcpy(shm, "hello world", 11); 
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{	
+	/* int tryid = -1;
+	key_t key;
+	key = 9976;
+    memcpy(shm, "hello world", 11);
 	s = shm;
+	 */
+	 
+	char name[80];
+	int size = 0;
+	
 	int val=1;
 	while(val)
 	{
-		sleep(1);
-		if(*shm == 'A')
-		{   
-			//printf("removed successfully %c \n", *shm);
-			int remove = shmctl(shmid, IPC_RMID, NULL);
-			if(remove == -1)
-			{
-				perror("there was an error in removing \n");
-			} 
-			val =0;
+		printf("Enter a name to create a file \n");
+		fgets(name, sizeof (name), stdin);
+        dele_file(name);
+        printf ("Enter size: ");
+        scanf("%d \n", &size);
+		create(name, size);
+        s = shm;
+        for(s = shm; *s != 0 ; s++)
+		{
+		printf("%c", *s);
+		}
+        if(*shm == 'A')
+        {
+			printf("stop the program \n");
+			val = 0;
 		}
 	}	
 	return 0;
